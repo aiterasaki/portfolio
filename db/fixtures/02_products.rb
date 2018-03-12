afbda944443ca2339d99ab4bdc79c61269aa84a3
aiterasaki <minicoopers0716@hotmail.com>
2018-03-13 00:51:17 +0900

    index

--- db/fixtures/02_products.rb
+++ db/fixtures/02_products.rb
@@ -9,6 +9,19 @@ index = 1
 	  s.burden_of_shipping_fee = 1
 	  s.shipping_method = 1
 	  s.region_id = 1
+
+		LadiesCategory.seed do |s|
+			s.id = 1
+			s.name = "トップス"
+			s.category_id = 1
+			
+				Category.seed do |s|
+					s.id = 1
+					s.name = "レディース"
+				end
+		end
+
+
 	  s.estimated_shipping_date = 1
 	  s.price = 1000 + index
 	  s.sell_flg = true

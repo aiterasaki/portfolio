d159d102811cc511a4da509d6764207ce673ddfc
aiterasaki <minicoopers0716@hotmail.com>
2018-03-21 13:05:45 +0900

    product/show view 修正

--- db/fixtures/04_product_images.rb
+++ db/fixtures/04_product_images.rb
@@ -1,16 +1,51 @@
-20.times do |num|
+20.times do |ladies|
 	ProductImage.seed do |i|
-		i.product_id = num
+		i.product_id = ladies
 		i.image = Rails.root.join("public/images/ladies_thumbnail_2.jpg").open
 	end
 
 	ProductImage.seed do |i|
-		i.product_id = num
+		i.product_id = ladies
 		i.image = Rails.root.join("public/images/ladies_thumbnail_3.jpg").open
 	end
 
 	ProductImage.seed do |i|
-		i.product_id = num
+		i.product_id = ladies
 		i.image = Rails.root.join("public/images/ladies_thumbnail_4.jpg").open
 	end
 end
+
+20.times do |mens|
+	ProductImage.seed do |i|
+		i.product_id = mens + 20
+		i.image = Rails.root.join("public/images/mens_thumbnail_2.jpg").open
+	end
+
+	ProductImage.seed do |i|
+		i.product_id = mens + 20
+		i.image = Rails.root.join("public/images/mens_thumbnail_3.jpg").open
+	end
+
+	ProductImage.seed do |i|
+		i.product_id = mens + 20
+		i.image = Rails.root.join("public/images/mens_thumbnail_4.jpg").open
+	end
+end
+
+20.times do |hand_made|
+	ProductImage.seed do |i|
+		i.product_id = hand_made + 40
+		i.image = Rails.root.join("public/images/hand_made_thumbnail_2.jpg").open
+	end
+
+	ProductImage.seed do |i|
+		i.product_id = hand_made + 40
+		i.image = Rails.root.join("public/images/hand_made_thumbnail_3.jpg").open
+	end
+
+	ProductImage.seed do |i|
+		i.product_id = hand_made + 40
+		i.image = Rails.root.join("public/images/hand_made_thumbnail_4.jpg").open
+	end
+end
+

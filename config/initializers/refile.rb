5982bdbe92e6ba7d6556cf733866453614ab7b82
aiterasaki <minicoopers0716@hotmail.com>
2018-06-01 12:36:04 +0900

    refile.rb修正

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -4,8 +4,10 @@ aws = {
   access_key_id: ENV['ACCESS_KEY_ID'],
   secret_access_key: ENV['SECRET_ACCESS_KEY'],
   region: "ap-northeast-1",
-  bucket: "portfolio201804"
+  bucket: "portfolio2018backup"
 }
-
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
+
+
+
